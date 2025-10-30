fa74ace617dc758774d2774c19c5b59590e2d7f2
gxickx <mgrokoe@gmail.com>
2025-10-30 10:01:57 -0300

    Se comenzó a desarrollar el requerimiento 1, agregando los renderizadores y viendo cómo seguir implementando el metodo factory

--- src/servicio/RenderizadorFactory.java
+++ src/servicio/RenderizadorFactory.java
@@ -6,13 +6,11 @@ public class RenderizadorFactory {
 
     public static IRenderizador crearRenderizador(String tipo) {
 
-        // Validamos la entrada
+
         if (tipo == null || tipo.isEmpty()) {
             throw new IllegalArgumentException("El tipo de renderizador no puede ser nulo o vacío.");
         }
 
-        // Usamos un 'switch' para decidir qué objeto concreto crear
-        // [cite: 17]
         switch (tipo.toUpperCase()) {
             case "PDF":
                 return new RenderizadorPDF();
@@ -21,7 +19,6 @@ public class RenderizadorFactory {
             case "CSV":
                 return new RenderizadorCSV();
             default:
-                // Es buena práctica lanzar una excepción si el tipo no se conoce
                 throw new IllegalArgumentException("Tipo de renderizador no soportado: " + tipo);
         }
     }
